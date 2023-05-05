a55ce7db27aa8c4cec46c29f2d5e1c6d04e79d14
Nizar Khatri <nizar_khatri@hotmail.com>
2023-05-05 22:48:00 +0500

    social navigation

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,14 @@
 import styles from '../../../styles/Footer.module.css';
 import { Container, Row, Col, Button } from 'reactstrap';
 import Image from 'next/image';
-import { FaGooglePlay, FaApple, FaTwitter, FaFacebook, FaInstagram, FaGithub } from 'react-icons/fa';
+import {
+  FaGooglePlay,
+  FaApple,
+  FaTwitter,
+  FaFacebook,
+  FaInstagram,
+  FaGithub,
+} from 'react-icons/fa';
 import Link from 'next/link';
 
 export const Footer = () => {
@@ -9,68 +16,102 @@ export const Footer = () => {
     <footer className={styles.footer}>
       <Container>
         <Row>
-        <Col md={3}>
-        <div className={`${styles.logoSection} ${styles.responsiveFooter}`}>
-          <img alt="logo" src={'/images/seywing-footer.svg'}  />
-          <p>Making the world a better place through constructing elegant hierarchies</p>
-          <div className={styles.socialMediaContainer}>
-            <ul>
-              <li><FaTwitter size={24} color="#FFFFFF" /></li>
-              <li><FaFacebook size={24} color="#FFFFFF" /></li>
-              <li><FaInstagram size={24} color="#FFFFFF" /></li>
-              <li><FaGithub size={24} color="#FFFFFF" /></li>
-            </ul>
-        </div>
-        </div>
-        </Col>
+          <Col md={3}>
+            <div className={`${styles.logoSection} ${styles.responsiveFooter}`}>
+              <img alt="logo" src={'/images/seywing-footer.svg'} />
+              <p>Making the world a better place through constructing elegant hierarchies</p>
+              <div className={styles.socialMediaContainer}>
+                <ul>
+                  <li>
+                      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                        <FaTwitter size={24} color="#FFFFFF" />
+                      </a>
+                  </li>
+                  <li>
+                      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                        <FaFacebook size={24} color="#FFFFFF" />
+                      </a>
+                  </li>
+                  <li>
+                      <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram size={24} color="#FFFFFF" />
+                      </a>
+                  </li>
+                  <li>
+                      <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                        <FaGithub size={24} color="#FFFFFF" />
+                      </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </Col>
           <Col md={3} className={`${styles.companyPadLeft} ${styles.footerColRes}`}>
             <h5 className={styles.headingFooterColumn}>Company</h5>
             <ul className={styles.footerNaviagtion}>
-              <li><a href="about-us">About Us</a></li>
-              <li><a href="pricing">Pricing</a></li>
-              <li><a href="blog">Blog</a></li>
-              <li><a href="partner">Partners</a></li>
+              <li>
+                <a href="about-us">About Us</a>
+              </li>
+              <li>
+                <a href="pricing">Pricing</a>
+              </li>
+              <li>
+                <a href="blog">Blog</a>
+              </li>
+              <li>
+                <a href="partner">Partners</a>
+              </li>
             </ul>
           </Col>
           <Col md={3} className={`${styles.paddingLeft} ${styles.footerColRes}`}>
             <h5 className={styles.headingFooterColumn}>Support</h5>
             <ul className={styles.footerNaviagtion}>
-              <li><Link href="faq">FAQs</Link></li>
-              <li><Link href="contact-us">Contact Us</Link></li>
-              <li><Link href="terms-of-use">Term of Use</Link></li>
-              <li><Link href="terms-of-sales">Term of Sales</Link></li>
-              <li><Link href="privacy">Privacy</Link></li>
+              <li>
+                <Link href="faq">FAQs</Link>
+              </li>
+              <li>
+                <Link href="contact-us">Contact Us</Link>
+              </li>
+              <li>
+                <Link href="terms-of-use">Term of Use</Link>
+              </li>
+              <li>
+                <Link href="terms-of-sales">Term of Sales</Link>
+              </li>
+              <li>
+                <Link href="privacy">Privacy</Link>
+              </li>
             </ul>
           </Col>
           <Col md={3}>
-          <div className="">
-            <div className={styles.btnAlignment}>
-              <Button outline className={styles.btnFtr}>
-                <div className={styles.downloadBtn}>
-                <FaGooglePlay size={25} className={styles.ftrIcon} />
-                  <span>
-                    Download on
-                    <strong>Google Play</strong>
-                  </span> 
-                </div>
-              </Button>
-              <Button outline className={styles.btnFtr} >
-                <div className={styles.downloadBtn}>
-                <FaApple size={30} className={styles.ftrIcon} />
-                  <span>
-                    Download on
-                    <strong>App Store</strong>
-                  </span> 
-                </div>
-              </Button>
+            <div className="">
+              <div className={styles.btnAlignment}>
+                <Button outline className={styles.btnFtr}>
+                  <div className={styles.downloadBtn}>
+                    <FaGooglePlay size={25} className={styles.ftrIcon} />
+                    <span>
+                      Download on
+                      <strong>Google Play</strong>
+                    </span>
+                  </div>
+                </Button>
+                <Button outline className={styles.btnFtr}>
+                  <div className={styles.downloadBtn}>
+                    <FaApple size={30} className={styles.ftrIcon} />
+                    <span>
+                      Download on
+                      <strong>App Store</strong>
+                    </span>
+                  </div>
+                </Button>
+              </div>
             </div>
-          </div>
-        </Col>
-        <Col md={12}>
-          <div className={styles.copyright} >
-            <p>© 2020 Dlex, Inc. All rights reserved</p>
-          </div>
-        </Col>
+          </Col>
+          <Col md={12}>
+            <div className={styles.copyright}>
+              <p>© 2020 Dlex, Inc. All rights reserved</p>
+            </div>
+          </Col>
         </Row>
       </Container>
     </footer>
